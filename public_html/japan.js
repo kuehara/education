e9b01cd55024e2907c7e34fd42a389336c069877
kuehara <parasan.mail@gmail.com>
2014-08-01 20:48:24 +0900

    default状態

--- public_html/japan.js
+++ public_html/japan.js
@@ -1,6 +1,7 @@
 var w = 1000;
 var h = 1000;
- 
+var cycle = [12,22,30,40,54];
+
 // SVG要素生成
 var svg = d3.select("body")
           .append("svg")
@@ -21,16 +22,52 @@ d3.json("./japan.json", function(json) {
     // 緯度経度⇒パスデータ変換設定
     var path = d3.geo.path()
         .projection(projection);
- 
     // パスデータとして日本地図描画
-    var color = d3.scale.category10();
     svg.selectAll("path")
-    .data(japan)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("stroke", "black")
-    .attr("stroke-width", 0.5)
-    .style("fill", "#2C2C43");
+        .data(japan)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("stroke", "black")
+        .attr("stroke-width", 0.5)
+        .style("fill", "#2C2C43");
 
+    svg.selectAll("circle")
+        .data(japan)
+        .enter()
+        .append("circle")
+        .transition()
+        .delay(function(d, i) {
+            return i * 300;
+        })
+        .duration(1000)
+        .each("start", function() {
+            d3.select(this)
+            .attr({
+                fill: "green",
+                r: 0,
+                cy: h
+            })
+        })
+        .attr({
+            r: 10,
+            fill: "yellow",
+            stroke: "orange"
+        })
+        .attr("stroke-width", 10)
+        .each("end", function() {
+            d3.select(this)
+            .transition()
+            .duration(800)
+            .attr({
+                fill: "ping",
+                r: 0,
+                cy: 10
+            })
+            .attr("transform", function(d) {
+                var lat = d.properties.latitude;
+                var lng = d.properties.longitude;
+                return "translate(" + projection([lng, lat]) + ")";
+            });
+        });
 });
